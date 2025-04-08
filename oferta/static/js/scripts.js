36e53e68f1b5bfd9180c299a79ccea81126e12db
AlexandreEscola <alcoliveira@es-loule.edu.pt>
2025-04-08 23:22:13 +0100

    Add background color transition effect on page load

--- oferta/static/js/scripts.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.body.style.backgroundColor = "#e0f7fa";
-    setTimeout(() => {
-        document.body.style.backgroundColor = "#f4f4f4";
-    }, 1000);
-});
